496ec3f73055bb241b97cbb2ca247db8e7cc9dc3
Jojosunderland <jojolucysunderland@gmail.com>
2025-03-30 15:22:09 +0100

    updated annotations

--- Coursework/Final_Meta_Analysis.R
+++ Coursework/Final_Meta_Analysis.R
@@ -16,10 +16,10 @@ data$elev_c <- data$elevation - mean(data$elevation)
 View(data)
 
 # Remove NAs from each start/end column
-ss_clean <- data[!is.na(data$spring_start), ] 
-sc_clean <- data[!is.na(data$spring_comp), ] 
-fs_clean <- data[!is.na(data$fall_start), ] 
-fc_clean <- data[!is.na(data$fall_comp), ] 
+ss_clean <- data[!is.na(data$spring_start), ] # ss = Spring start
+sc_clean <- data[!is.na(data$spring_comp), ] # sc = Spring completion
+fs_clean <- data[!is.na(data$fall_start), ] # fs = fall start
+fc_clean <- data[!is.na(data$fall_comp), ] # fc = fall completion
 
 ########################## META ANALYSES ##############################
 
@@ -51,6 +51,9 @@ fc_metayear <- rma.mv(yi = fall_comp, V = fc_se^2, mods = ~ year_c + species + l
 # random effects models for whole effect size
 
 # Fit a model WITHOUT moderators (random-effects model)
+
+## SPRING ##
+# spring start #
 random_model_ss <- rma.mv(yi = ss_ordered_year$spring_start, 
                     V = ss_ordered_year$ss_se^2,  
                     random = list(~1|site, ~1|study), 
@@ -59,6 +62,7 @@ random_model_ss <- rma.mv(yi = ss_ordered_year$spring_start,
 est_ss <- as.numeric(coef(random_model_ss)) # Overall effect estimate
 se_ss <- random_model_ss$se    # Standard error
 
+# spring end #
 random_model_sc <- rma.mv(yi = sc_ordered_year$spring_comp, 
                           V = sc_ordered_year$sc_se^2,  
                           random = list(~1|site, ~1|study),
@@ -66,7 +70,8 @@ random_model_sc <- rma.mv(yi = sc_ordered_year$spring_comp,
 est_sc <- as.numeric(coef(random_model_sc))     
 se_sc <- random_model_sc$se    
 
-
+## FALL ##
+# fall start #
 random_model_fs <- rma.mv(yi = fs_ordered_year$fall_start, 
                           V = fs_ordered_year$fs_se^2,  
                           random = list(~1|site, ~1|study),
@@ -74,6 +79,7 @@ random_model_fs <- rma.mv(yi = fs_ordered_year$fall_start,
 est_fs <- as.numeric(coef(random_model_fs))           
 se_fs <- random_model_fs$se   
 
+# fall end #
 random_model_fc <- rma.mv(yi = fc_ordered_year$fall_comp, 
                           V = fc_ordered_year$fc_se^2,  
                           random = list(~1|site, ~1|study),
@@ -83,29 +89,31 @@ se_fc <- random_model_fc$se
 
 
 ################################# PLOTS #####################################
-# forest plots - in year order:
 
-#allocate colours to species
 
+# Allocate colours to species for plots #
+
+# spring start
 species_col_ss <- c("Lepus americanus" = "slateblue3",
                  "Lepus timidus" = "lightblue")
-
 row_colours_ss <- species_col_ss[ss_ordered_year$species]
 
+# spring end 
 species_col_sc <- c("Lepus americanus" = "slateblue3",
                     "Lepus timidus" = "lightblue")
-
 row_colours_sc <- species_col_sc[sc_ordered_year$species]
 
+# fall start 
 species_col_fs <- c("Lepus americanus" = "slateblue3",
                     "Lepus timidus" = "lightblue")
-
 row_colours_fs <- species_col_fs[fs_ordered_year$species]
 
+# fall end
 species_col_fc <- c("Lepus americanus" = "slateblue3",
                     "Lepus timidus" = "lightblue")
-
 row_colours_fc <- species_col_fc[fc_ordered_year$species]
+
+###### forest plots - in year order: ####
 
 # SPRING FOREST PLOT #
 quartz()
@@ -116,8 +124,7 @@ forest(ss_metayear, cex.lab=0.8, cex.axis=0.8,addfit=FALSE,shade="zebra",
        slab = year, border = NA, xlim = c(0,250),
        ylim = c(-2, ss_metayear$k + 3), header = 'Year',
        colout = row_colours_ss)
-
-# Add summary diamond of random effect model 
+# Add random effect model 
 abline(h = -0, col = "black", lwd = 1.5)
 addpoly.default(x = est_ss, sei = se_ss, rows = -1, col = "#5CC8A7", mlab = "Random Effects Model", cex = 0.8)
 
